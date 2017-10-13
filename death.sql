fb270a081c3f81100b5c1101463587f0e30e1af2
TheSmoothOperator <thesmoothoperator@gmail.com>
2017-10-12 23:41:04 -0400

    fixed exile-addon.ini to take the new inputs. Added new table columns and the event to add the distance.

--- death.sql
+++ death.sql
@@ -5,8 +5,14 @@ CREATE TABLE `death` (
 	`position_x` DOUBLE NOT NULL,
 	`position_y` DOUBLE NOT NULL,
 	`position_z` DOUBLE NOT NULL,
+	`weapon` VARCHAR(255) NULL,
+	`victimNear` VARCHAR(255) NULL.
+	`killer_x` DOUBLE NULL,
+	`killer_y` DOUBLE NULL,
+	`killer_z` DOUBLE NULL,
 	`killedWhen` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
 	`systemChat` VARCHAR(255) NULL DEFAULT NULL,
+	`distance` DOUBLE NULL,
 	PRIMARY KEY (`deathID`),
 	INDEX `FK_account_BY` (`killedBy`),
 	INDEX `FK__account` (`uID`),
@@ -16,3 +22,10 @@ CREATE TABLE `death` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
+
+
+CREATE DEFINER=`insertdatabasename`@`%` TRIGGER `death_before_insert` BEFORE INSERT ON `death` FOR EACH ROW BEGIN
+    
+    SET New.distance = SQRT(POW(NEW.killer_x-NEW.position_x,2)+POW(NEW.killer_y-NEW.position_y,2));
+    
+END
